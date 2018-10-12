2d65eb0a6d1c2b3f497a5a708855cc4e5263bf85
DMXMax <29312988+DMXMax@users.noreply.github.com>
2018-10-12 09:50:38 +0000

    Better validation

--- base.go
+++ base.go
@@ -58,10 +58,19 @@ func (er *ExternalReference) IsValid() (bool, error){
 
 type ObjectIdentifier string
 
-func CreateID(objectType string) (id ObjectIdentifier){
+func NewObjectId(objectType string) ObjectIdentifier{
   var buf strings.Builder
-  fmt.Fprintf(&buf, "%s--%s", objectType, uuid.New().String())
-  id = ObjectIdentifier(buf.String())
-  return
+  fmt.Fprintf(&buf, "%s--%s", strings.ToLower(objectType), uuid.New().String())
+  return ObjectIdentifier(buf.String())
 }
 
+func (o *ObjectIdentifier) Valid() (err error){
+  err = nil
+  strs := strings.Split(string(*o),"--")
+  if len(strs)<2{ // we need an object type and a uuid
+    err = errors.New("The id isn't right")
+  }else{
+    _,err = uuid.Parse(strs[1])//the second part should be the uuid
+  }
+  return
+}
